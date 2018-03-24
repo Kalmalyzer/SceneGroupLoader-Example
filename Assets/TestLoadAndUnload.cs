40281d394b1f8312ec1f967e8ec80e68a470bb45
Mikael Kalms <mikael@kalms.org>
2018-03-24 20:16:32 +0100

    Setup load/unload test (currently hitting errors in loader)

---
+++ Assets/TestLoadAndUnload.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using SceneGroupLoader;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class TestLoadAndUnload : MonoBehaviour {
+
+    public SceneGroupLoaderComponent Loader;
+
+    public SceneGroup SceneGroup1;
+
+    void Awake()
+    {
+        Assert.IsNotNull(Loader);
+        Assert.IsNotNull(SceneGroup1);
+    }
+
+	// Use this for initialization
+	void Start () {
+
+        Loader.LoadSceneGroup(SceneGroup1, OnSceneGroup1Loaded);
+	}
+
+    private void OnSceneGroup1Loaded(SceneGroupLoader.SceneGroupLoader.SceneGroupHandle sceneGroupHandle)
+    {
+        Loader.ActivateSceneGroup(sceneGroupHandle, OnSceneGroup1Activated);
+    }
+
+    private void OnSceneGroup1Activated(SceneGroupLoader.SceneGroupLoader.SceneGroupHandle sceneGroupHandle)
+    {
+        Loader.UnloadSceneGroup(sceneGroupHandle, OnSceneGroup1Unloaded);
+    }
+
+    private void OnSceneGroup1Unloaded(SceneGroupLoader.SceneGroupLoader.SceneGroupHandle sceneGroupHandle)
+    {
+        Debug.Log("Done");
+    }
+}
